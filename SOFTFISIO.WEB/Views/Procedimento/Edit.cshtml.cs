84cbd7a9d79ae59018d6fdf704242e0b17e8125d
menezesoliveira40 <menezesoliveira40@hotmail.com>
2026-06-19 16:41:24 -0300

    Correções de Erros

--- SOFTFISIO.WEB/Views/Procedimento/Edit.cshtml.cs
+++ SOFTFISIO.WEB/Views/Procedimento/Edit.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace SOFTFISIO.WEB.Views.Equipamento
+namespace SOFTFISIO.WEB.Views.Procedimento
 {
     public class EditModel : PageModel
     {
